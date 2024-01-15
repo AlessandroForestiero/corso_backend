3cf96842187bf42b586fd081f24e610970376774
AlessandroForestiero <forestieroalessandro94@gmail.com>
2024-01-15 17:48:37 +0100

    esercizio2

---
+++ src/week2/esercizio2.java
@@ -0,0 +1,27 @@
+package week2;
+import java.sql.SQLOutput;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class esercizio2 {
+    private static final Logger logger=LoggerFactory.getLogger(esercizio2.class);
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        try {
+            System.out.println("inserisci numero kilometri percorso");
+            int x = scanner.nextInt();
+            System.out.println("inserisci carburante consumato");
+            int y = scanner.nextInt();
+            System.out.println(x/y);
+        }
+        catch (ArithmeticException  e) {
+            System.out.println("non posso dividere per zero");
+            logger.error("non posso dividere per zero");
+        }
+        catch (InputMismatchException e){
+            System.out.println("devi inserire un numero");
+            logger.error("devi inserire un numero");
+        }
+    }
+}
