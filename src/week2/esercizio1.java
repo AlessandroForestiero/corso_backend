bd27ca920d5fdf170719133e387b2f2285fde3de
AlessandroForestiero <forestieroalessandro94@gmail.com>
2024-01-15 17:02:59 +0100

    esercizio1

---
+++ src/week2/esercizio1.java
@@ -0,0 +1,39 @@
+package week2;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class esercizio1 {
+
+    private static final Logger logger=LoggerFactory.getLogger(esercizio1.class);
+    private static int[] valoriCasuali = new int[]{1, 5, 7, 8, 3};
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = -1;
+        while (x != 0) {
+            try {
+                System.out.println("inserisci un numero");
+                x = scanner.nextInt();
+                if (x != 0) {
+                    System.out.println("inserisci una posizione");
+                    int y = scanner.nextInt();
+                    valoriCasuali[y] = x;
+                    System.out.println(Arrays.toString(valoriCasuali));
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("posizione non concessa");
+                logger.error("posizione non concessa");
+            } catch (InputMismatchException e) {
+                System.out.println("numero non valido");
+                logger.error("numero non valido");
+                scanner.nextLine();
+            }
+
+        }
+    }
+}
+
