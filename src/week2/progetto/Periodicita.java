b1df8c36ba90f9a16309027b20a9db6bd0f1b05a
AlessandroForestiero <forestieroalessandro94@gmail.com>
2024-01-19 16:52:13 +0100

    progetto

---
+++ src/week2/progetto/Periodicita.java
@@ -0,0 +1,7 @@
+package week2.progetto;
+
+public enum Periodicita {
+    SETTIMANALE,
+    MENSILE,
+    SEMESTRALE
+}
---
+++ src/week2/progetto/Rivista.java
@@ -0,0 +1,26 @@
+package week2.progetto;
+
+public class Rivista extends ArticoloCartaceo {
+
+    private Periodicita periodicita ;
+    public Rivista(String isbn, String titolo, int annoPubblicazione, int numeroPagine, Periodicita periodicita) {
+        super(isbn,titolo,annoPubblicazione,numeroPagine);
+        this.periodicita=periodicita;
+    }
+
+    public Periodicita getPeriodicita() {
+        return periodicita;
+    }
+
+    public void setPeriodicita(Periodicita periodicita) {
+        this.periodicita = periodicita;
+    }
+    @Override
+    public String toString() {
+        return  "isbn: " + this.getIsbn()+
+                " titolo: " + this.getTitolo() +
+                " Anno di pubblicazione : " + this.getAnnoPubblicazione() +
+                " numero di pagine : " + this.getNumeroPagine() +
+                " periodicità: " + this.periodicita;
+    }
+}
